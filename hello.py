96733493f0080f1f6edafbbb914da866b29e3ef4
Eddie Gonzalez <Eddie@OfeliaGalezsMBP.fios-router.home>
2020-12-22 00:12:05 -0800

    added hello.py

---
+++ hello.py
@@ -0,0 +1,11 @@
+# Filename: hello.py
+"""Simple Hello World example iwth PyQt5"""
+
+import sys
+
+# 1. Import 'QApplication' and all the required widgets
+
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget
+
+# 2. Create an instance of QApplication
+app = QApplication(sys.argv)
